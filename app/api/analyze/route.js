058b31c021529c30927b964394e266b942a361ed
Felicia Ivanna <2472030@maranatha.ac.id>
2026-05-10 18:13:18 +0700

    solve prob on route.js

--- app/api/analyze/route.js
+++ app/api/analyze/route.js
@@ -50,7 +50,7 @@ ${text}
 `;
 
     const response = await ai.models.generateContent({
-      model: 'gemini-1.5-pro',
+      model: 'gemini-2.0-flash-lite',
       contents: [
         {
           role: 'user',
--- app/api/imagetruth/route.js
+++ app/api/imagetruth/route.js
@@ -50,7 +50,7 @@ Pastikan score dan riskLevel HARUS konsisten:
 `;
 
     const response = await ai.models.generateContent({
-      model: 'gemini-1.5-pro',
+      model: 'gemini-2.0-flash-lite',
       contents: [
         {
           role: 'user',
